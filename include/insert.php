ab73664e3e72b77eb0011727e0eac54b78ef84de
fafafirafa <39588133+fafafirafa@users.noreply.github.com>
2020-08-11 01:18:01 +0200

    Update insert.php

--- include/insert.php
+++ include/insert.php
@@ -1,6 +1,7 @@
 <?php
 /*
-$tak = mysqli_real_escape_string($mysqli, 'Oddaję do waszego użytku opcję przejścia na następną bądź poprzednią stronę na liście zablokowanych osób. W razie jakichkolwiek błędów/problemów proszę zgłaszać na discord: Bercik#9999');
+include 'connection.php';
+$tak = mysqli_real_escape_string($mysqli, 'XXX');
 if($mysqli->query("INSERT INTO `ogloszenia`(`Tresc`) VALUES ('$tak')")){
     echo "Tak";
 } else {
