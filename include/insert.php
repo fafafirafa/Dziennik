83f05c00295d9c3a7ec3b7169a3043faadc589a0
Bercik <berciklbn@op.pl>
2020-08-27 19:23:19 +0200

    Bercik

--- include/insert.php
+++ include/insert.php
@@ -1,7 +1,7 @@
 <?php
 /*
 include 'connection.php';
-$tak = mysqli_real_escape_string($mysqli, 'XXX');
+$tak = mysqli_real_escape_string($mysqli, 'Wiadomość');
 if($mysqli->query("INSERT INTO `ogloszenia`(`Tresc`) VALUES ('$tak')")){
     echo "Tak";
 } else {
