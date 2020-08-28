e0d2067b1703c25fb438342f19f45505ea34c738
Bercik <berciklbn@op.pl>
2020-08-28 21:34:33 +0200

    Bercik
    Bugfix

--- include/delete.php
+++ include/delete.php
@@ -1,25 +1,29 @@
 <?php
 include 'connection.php';
 
-    if(Isset($_POST['id'])) {
-        $Id = mysqli_real_escape_string($mysqli, $_POST['id']);
-        $usr = $mysqli->query("SELECT `Nadajacy` FROM `wykluczenia` WHERE Id=$Id");
-        if($usr->num_rows > 0){
-            while($row = $usr->fetch_assoc()){
-                $user = $row['Nadajacy'];
-            }
-        }
-        $count = $mysqli->query("SELECT `Wpisy` FROM `uzytkownicy` WHERE `User` = '$user'");
-        if($count->num_rows > 0) {
-            while($row = $count->fetch_assoc()){
-                $count = $row['Wpisy'];
-            }
-        }
+if (isset($_POST['id'])) {
+    $Id = mysqli_real_escape_string($mysqli, $_POST['id']);
+    $usr = $mysqli->query("SELECT `Nadajacy` FROM `wykluczenia` WHERE Id=$Id");
+    if ($usr->num_rows > 0) {
+        $row = $usr->fetch_assoc();
+        $user = $row['Nadajacy'];
+    }
+    $count = $mysqli->query("SELECT `Wpisy` FROM `uzytkownicy` WHERE `User` = '$user'");
+    if ($count->num_rows > 0) {
+        $row = $count->fetch_assoc();
+        $count = $row['Wpisy'];
+
         $count--;
-        if($mysqli->query("DELETE FROM `wykluczenia` WHERE Id=$Id")){
-            $mysqli->query("UPDATE `uzytkownicy` SET `Wpisy` = '$count' WHERE `User` = '$nadajacy'");
+    } else {
+        $count = 0;
+    }
+    if ($mysqli->query("DELETE FROM `wykluczenia` WHERE Id=$Id")) {
+        if ($mysqli->query("UPDATE `uzytkownicy` SET `Wpisy` = '$count' WHERE `User` = '$user'")) {
             echo "Pomyślnie usunięto";
         } else {
             printf($mysqli->error);
         }
+    } else {
+        printf($mysqli->error);
     }
+}
